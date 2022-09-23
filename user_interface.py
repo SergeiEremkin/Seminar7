a8f9dae847664d8f676015c09f4187b8fbf82610
Oxana <crazyglass@list.ru>
2022-09-23 15:11:17 +0300

    Доделан модули юзера и проверок, связаны модули проверки, юзера и вычисления рациональных чисел

--- user_interface.py
+++ user_interface.py
@@ -1,25 +1,31 @@
-import controller
+import check as ch
 
 def view_data(result, expressiontitle:str)->str:
     '''
     Вывод результата
-    '''
-    
+    '''    
     print(f'{expressiontitle} = {result}')
 
-def get_complex(string_input: str)-> int:
+def get_rational(string_input: str)-> float:
     '''
-    Ввод комплексного числа 
+    Ввод рационального числа
     '''
-    return int(string_input)
+    number = ch.get_number_float(string_input)
+    return number
 
-def get_rational(string_input: str)-> str:
-    '''
-    Ввод рационального
-    '''
-    return input(string_input)
-
-def get_oper()-> str:
+def get_oper(string_input: str)-> str:
     '''
     Выбор операции
     '''
+    symbol = ch.get_symbol(string_input)
+    return symbol
+
+def get_choice(string_input: str)-> int:
+    '''
+    Выбор действия или вида чисел
+    '''
+    choice = ch.get_selection(string_input)
+    return choice
+
+# num = get_choice('Введите число\n:')
+# print(num)
